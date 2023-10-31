114b5f4d3f5aab134b95acac2999583d0a064ad7
Anna-Chernukhina <sakuretka27@gmail.com>
2023-10-31 12:14:12 +0000

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
 
+const buttonStart = document.querySelector("[data-start]");
+const buttonStop = document.querySelector("[data-stop]");
+
+buttonStart.addEventListener("click", startChangeBackground);
+buttonStop.addEventListener("click", stopChangeBackground);
+
+let intervalId;
+
+function startChangeBackground() {
+    document.body.style.backgroundColor = getRandomHexColor();
+    buttonStart.disabled = true;
+    buttonStop.disabled = false;
+    intervalId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+}
+
+function stopChangeBackground() {
+    clearInterval(intervalId);
+    buttonStart.disabled = false;
+    buttonStop.disabled = true;
+}
